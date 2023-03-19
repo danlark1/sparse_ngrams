b15ae84b9220bdf63d62664568948ac9f7f58bfc
Danila Kutenin <kutdanila@yandex.ru>
2023-03-19 23:53:35 +0000

    Remove unneeded hack for now

--- src/sparse_ngrams.cc
+++ src/sparse_ngrams.cc
@@ -11,7 +11,6 @@ namespace {
 // Some hashing algorithm.
 inline uint32_t HashBigram(const char* begin) {
   // Improving the efficiency of ^ and $ searches.
-  if (begin[0] == '\n' || begin[1] == '\n') return 0;
   const uint64_t kMul1 = 0xc6a4a7935bd1e995ULL;
   const uint64_t kMul2 = 0x228876a7198b743ULL;
   uint64_t a =
@@ -38,7 +37,7 @@ void SparseNgramsBuilder::BuildAllNgrams(
     HashAndPos p{HashBigram(s.data() + i), i + 1};
     while (!st.empty() && p.hash < st.back().hash) {
       consumer(s.data() + st.back().pos - 1, s.data() + i + 2);
-      while (st.size() > 1 && st.back().hash == (st.end() - 2)->hash) {
+      while (st.size() > 1 && st.back().hash == st[st.size() - 2].hash) {
         st.pop_back();
       }
       st.pop_back();
@@ -58,32 +57,32 @@ void SparseNgramsBuilder::BuildCoveringNgrams(
     uint32_t hash;
     size_t pos;
   };
-  std::deque<HashAndPos> st;
+  std::deque<HashAndPos> deque;
   // Look at increasing and decreasing sequences.
   for (size_t i = 0; i + 2 <= s.size(); ++i) {
     HashAndPos p{HashBigram(s.data() + i), i + 1};
     // Filter out big ngrams.
-    if (!st.empty() && i - st.front().pos + 3 >= options_.max_ngram_size) {
-      consumer(s.data() + st[0].pos - 1, s.data() + st[1].pos + 1);
-      st.pop_front();
+    if (!deque.empty() && i - deque.front().pos + 3 >= options_.max_ngram_size) {
+      consumer(s.data() + deque[0].pos - 1, s.data() + deque[1].pos + 1);
+      deque.pop_front();
     }
-    while (!st.empty() && p.hash < st.back().hash) {
-      if (st.front().hash == st.back().hash) {
-        consumer(s.data() + st.back().pos - 1, s.data() + i + 2);
-        while (st.size() > 1) {
-          size_t last_position = st.back().pos;
-          st.pop_back();
-          consumer(s.data() + st.back().pos - 1, s.data() + last_position + 1);
+    while (!deque.empty() && p.hash < deque.back().hash) {
+      if (deque.front().hash == deque.back().hash) {
+        consumer(s.data() + deque.back().pos - 1, s.data() + i + 2);
+        while (deque.size() > 1) {
+          size_t last_position = deque.back().pos;
+          deque.pop_back();
+          consumer(s.data() + deque.back().pos - 1, s.data() + last_position + 1);
         }
       }
-      st.pop_back();
+      deque.pop_back();
     }
-    st.push_back(p);
+    deque.push_back(p);
   }
-  while (st.size() > 1) {
-    size_t last_position = st.back().pos;
-    st.pop_back();
-    consumer(s.data() + st.back().pos - 1,
+  while (deque.size() > 1) {
+    size_t last_position = deque.back().pos;
+    deque.pop_back();
+    consumer(s.data() + deque.back().pos - 1,
              s.data() + last_position + 1);
   }
 }
